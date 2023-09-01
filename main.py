86138c35a263fcfcda8e142a5c5af476fabea0cc
Jay Sim <yourthought2020@gmail.com>
2023-08-31 20:01:42 -0700

    Add breakline ln=1

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ for filepath in filepaths:
     filename = Path(filepath).stem
     invoice_nr, date = filename.split("-")
     pdf.set_font(family="Times", size=16, style="B")
-    pdf.cell(w=50, h=8, txt=f"Invoice nr.{invoice_nr}")
+    pdf.cell(w=50, h=8, txt=f"Invoice nr.{invoice_nr}", ln=1)
 
     pdf.set_font(family="Times", size=16, style="B")
     pdf.cell(w=50, h=8, txt=f"Date: {date}")
